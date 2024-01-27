10901464e1d191032c9913525316287aff25ae1e
SeNoRQA <153427821+SeNoRQA@users.noreply.github.com>
2024-01-27 18:08:41 +0300

    Update main.py

--- PythonProjects/RequestsTest/main.py
+++ PythonProjects/RequestsTest/main.py
@@ -1,37 +1,35 @@
 
 import requests
 
-URL = 'https://api.pokemonbattle.me:9104'
-headers = {
-    "Content-Type": "application/json",
-    "trainer_token": "349c11c01fd49132546600dcc8d52596",
-}
+response = requests.post(url='https://api.pokemonbattle.me:9104/pokemons', 
+              json={
+                  "name": "generate",
+                   "photo": "generate"
+                   },
+               headers={
+                   'Content-Type': 'application/json', 
+                  'trainer_token':'349c11c01fd49132546600dcc8d52596'
+                       })
+print(f'Code:{response.status_code} {response.reason}. Message:{response.text}')
 
 
-#body1 = {
-#    "name": "Бартан",
-#    "photo": "https://dolnikov.ru/pokemons/albums/045.png"
-    
-#}
-#response = requests.post(url=f'{URL}/pokemons', json=body1, headers=headers, timeout=5)
-#print(response)
+response = requests.put(url='https://api.pokemonbattle.me:9104/pokemons', 
+              json={
+                   "pokemon_id": "28357",
+                  "name": "Prode_Main",
+                   "photo": "https://dolnikov.ru/pokemons/albums/.088png"
+                    },
+               headers={
+                   'Content-Type': 'application/json', 
+                   'trainer_token':'#YOUR_TOKEN'
+                       })
+print(f'Code:{response.status_code} {response.reason}. Message:{response.text}')
 
-
-#body2 = {
-#    "pokemon_id": "28357",
-#   "name": "ТЫнсТЫнс",
-#    "photo": "https://dolnikov.ru/pokemons/albums/038.png"
-#}
-
-
-
-# requests.put(url=f'{URL}/pokemons', json=body2, headers=headers, timeout=5)
-#print(response)
-
-
-#body3 = {
-#   "pokemon_id": "28357"
-#}
-
-# response = requests.post(url=f'{URL}/trainers/add_pokeball', json=body3, headers=headers, timeout=5)
-#print(response)
+response = requests.post(url='https://api.pokemonbattle.me:9104/trainers/add_pokeball', 
+              json=
+              {"pokemon_id": "28357"},
+               headers={
+                  'Content-Type': 'application/json', 
+                   'trainer_token':'349c11c01fd49132546600dcc8d52596'
+                       })
+print(f'Code:{response.status_code} {response.reason}. Message:{response.text}')
